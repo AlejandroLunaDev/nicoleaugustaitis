de6343c4c229df957874b8f5cc5a3a6c8bc06e8b
AlejandroLunaDev <alejandrolunadev@gmail.com>
2024-11-26 08:20:49 -0300

    chore: 🚧Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <section>
+      <h1 className="text-[40px] text-center text-primary font-parisienne">Nicole</h1>
+      <p className="text-[18px] text-secondary font-montserrat">hola todo bien</p>
+    </section>
+  );
+}
