e4e56ecde701cfec393ba1bbc53bb769a70eb8a2
AlejandroLunaDev <alejandrolunadev@gmail.com>
2024-11-29 17:00:54 -0300

    refactor: 🏗️carroucel reviews

--- features/reviews/Reviews.tsx
+++ features/reviews/Reviews.tsx
@@ -1,71 +1,29 @@
-'use client';
-import { reviews } from './reviewsData';
-import { useState } from 'react';
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
-import { renderStars } from './components/RenderStars';
-import { Card } from '@/shared/components/ui/card';
-import { Button } from '@/shared/components/ui/button';
+"use client";
 
-
-
-
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/shared/components/ui/carousel";
+import { reviews } from "./reviewsData";
+import { renderStars } from "./components/RenderStars";
 
 export default function ReviewsCarousel() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [direction, setDirection] = useState(true);
-
-  const handlePrev = () => {
-    setDirection(!direction);
-    setCurrentIndex(prevIndex =>
-      prevIndex === 0 ? reviews.length - 1 : prevIndex - 1
-    );
-  };
-
-  const handleNext = () => {
-    setDirection(!direction);
-    setCurrentIndex(prevIndex =>
-      prevIndex === reviews.length - 1 ? 0 : prevIndex + 1
-    );
-  };
-
   return (
-    <section
-      className='flex flex-col items-center gap-4 p-4'
-      id='depoimentos'
-    >
-      <header className='text-center'>
-        <h2 className='text-3xl font-semibold text-primary'>Depoimentos</h2>
+    <section className="flex flex-col items-center gap-4 p-4" id="depoimentos">
+      <header className="text-center">
+        <h2 className="text-3xl font-semibold text-primary">Depoimentos</h2>
+
       </header>
-      <Card className='w-full  p-2 text-center overflow-hidden lg:h-[400px]'>
-        <div className='flex flex-col items-center justify-between'>
-          <div className='mt-4'>
-            {renderStars(reviews[currentIndex].rating)}
-          </div>
-          <span className='text-xs lg:text-lg font-semibold'>
-            Avaliaçao 5.0 dos meus pacientes
-          </span>
-
-          <div className='flex items-center overflow-hidden mt-4'>
-            <Button onClick={handlePrev} variant='ghost'>
-              <FaChevronLeft size={24} />
-            </Button>
-
-            <div
-              key={currentIndex}
-             
-              className='flex flex-col items-center w-full mx-4 max-h-[150px] lg:h-[400px]  overflow-y-scroll lg:overflow-y-auto'
-            >
-              <p className='mt-2 text-left text-xs lg:text-xl text-primary'>
-                {reviews[currentIndex].text}
-              </p>
-            </div>
-
-            <Button onClick={handleNext} variant='ghost'>
-              <FaChevronRight size={24} />
-            </Button>
-          </div>
-        </div>
-      </Card>
+      <Carousel opts={{loop: true}} className="relative w-full max-h-[400px] mt-4">
+        <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2 z-10 bg-primary text-white p-2 rounded-full shadow-md" />
+        <CarouselContent className="flex w-full">
+          {reviews.map((review, index) => (
+            <CarouselItem key={index} className="flex flex-col items-center p-4 bg-white rounded-lg shadow-lg">
+              <div className="mt-4">{renderStars(review.rating)}</div>
+              <p className="font-semibold p-2">Avaliações 5.0 dos meus pacientes</p>
+              <p className="mt-2 text-sm lg:text-xl px-12 text-primary">{review.text}</p>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2 z-10 bg-primary text-white p-2 rounded-full shadow-md" />
+      </Carousel>
     </section>
   );
 }
