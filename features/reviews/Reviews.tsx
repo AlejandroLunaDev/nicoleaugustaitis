6c2e824d4faaa270668aa6815aa610244a3212c9
AlejandroLunaDev <alejandrolunadev@gmail.com>
2024-11-29 17:20:04 -0300

    fix: 🚧carroucel mobile text

--- features/reviews/Reviews.tsx
+++ features/reviews/Reviews.tsx
@@ -9,16 +9,26 @@ export default function ReviewsCarousel() {
     <section className="flex flex-col items-center gap-4 p-4" id="depoimentos">
       <header className="text-center">
         <h2 className="text-3xl font-semibold text-primary">Depoimentos</h2>
-
       </header>
-      <Carousel opts={{loop: true}} className="relative w-full max-h-[400px] mt-4">
+      <Carousel opts={{ loop: true }} className="relative w-full max-h-[400px] mt-4">
         <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2 z-10 bg-primary text-white p-2 rounded-full shadow-md" />
         <CarouselContent className="flex w-full">
           {reviews.map((review, index) => (
             <CarouselItem key={index} className="flex flex-col items-center p-4 bg-white rounded-lg shadow-lg">
               <div className="mt-4">{renderStars(review.rating)}</div>
               <p className="font-semibold p-2">Avaliações 5.0 dos meus pacientes</p>
-              <p className="mt-2 text-sm lg:text-xl px-12 text-primary">{review.text}</p>
+
+              {/* Condicional para truncar textos que superen 400 caracteres */}
+              <p
+                className={`
+                  mt-2 text-xs lg:text-xl px-12 text-primary 
+                  ${review.text.length > 350 ? 'line-clamp-6' : ''}  // Aplicar truncado solo si el texto es largo
+                  md:line-clamp-none  // No truncar en desktop
+                  sm:text-base
+                `}
+              >
+                {review.text}
+              </p>
             </CarouselItem>
           ))}
         </CarouselContent>
